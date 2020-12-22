0e6eee1cfa9434b4ccd2f25b1daa9fa514158bb1
Bk49 <ericngyongwei@gmail.com>
2020-12-22 14:33:17 +0800

    Fix uncaught exception

--- public/js/copy.js
+++ public/js/copy.js
@@ -13,12 +13,12 @@ function displayAlert(name) {
     "<span class='closebtn' onClick='removeAlert()'>&times;</span>"+
     name+" has been copied to clipboard!"+
   "</div><br>"
-  console.log(display.childElementCount)
   setTimeout(removeAlert, 5000)
 }
 
 function removeAlert() {
     let display = document.getElementById("render")
+    if(display.childElementCount <= 0) return;
     display.removeChild(display.childNodes[0])
     display.removeChild(display.childNodes[0])
 }
